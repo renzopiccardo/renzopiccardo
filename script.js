d2b05612ee9d4072949c56042b2c7d7e10d968dc
Renzo Piccardo <renzopiccardo98@gmail.com>
2018-11-06 23:09:26 -0300

    fds

--- script.js
+++ script.js
@@ -25,7 +25,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
-function filtrarJuegos() {
+function filtrarJuegos() 
+{
   // Variables
   var input, filter, ul, li, a, i;
   input = document.getElementById('filtroTexto');
@@ -34,55 +35,71 @@ function filtrarJuegos() {
   li = ul.getElementsByTagName('li');
 
   // Loopear entre todos los elementos de la lista y ocultar los que no coinciden con la busqueda
-  for (i = 0; i < li.length; i++) {
+  for (i = 0; i < li.length; i++) 
+  {
     a = li[i].getElementsByTagName("div")[0];
 
-    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) 
+    {
       li[i].style.display = "";
-    } else {
+    } 
+    else 
+    {
       li[i].style.display = "none";
     }
   }
 }
 
-function ordenarJuegos() {
+function ordenarJuegos() 
+{
   var list, i, switching, b, shouldSwitch, dir, switchcount = 0;
   list = document.getElementById("catalogo");
   switching = true;
   // orden ascendente
   dir = "asc";
   // Loop hasta que no se necesite mas switching
-  while (switching) {
+  while (switching) 
+  {
     // Empezar diciendo que no se va a hacer switching
     switching = false;
     b = list.getElementsByTagName("li");
     // Loopear entre todos los li
-    for (i = 0; i < (b.length - 1); i++) {
+    for (i = 0; i < (b.length - 1); i++) 
+    {
       shouldSwitch = false;
       // Checkear si el siguente li tendria que cambiar lugar con el actual
-      if (dir == "asc") {
-        if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+      if (dir == "asc") 
+      {
+        if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) 
+        {
           //Si el siguiente li es alfabeticamente menor que el actual, indicar switch y cortar el loop
           shouldSwitch = true;
           break;
         }
-      } else if (dir == "desc") {
-        if (b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase()) {
+      } 
+      else if (dir == "desc") 
+      {
+        if (b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase()) 
+        {
           //Si el siguiente li es alfabeticamente mayor que el actual, indicar switch y cortar el loop
           shouldSwitch = true;
           break;
         }
       }
     }
-    if (shouldSwitch) {
+    if (shouldSwitch) 
+    {
       //Si un switch fue marcado, hacer el switch e indicar que se hizo
       b[i].parentNode.insertBefore(b[i + 1], b[i]);
       switching = true;
       //Cada vez que se hace un swiitch, incrementar en 1
       switchcount++;
-    } else {
+    } 
+    else 
+    {
       // Si no se hizo switching y la direccion en ascendente, setear la direccion en descendente y hacer el loop de nuevo
-      if (switchcount == 0 && dir == "asc") {
+      if (switchcount == 0 && dir == "asc") 
+      {
         dir = "desc";
         switching = true;
       }
@@ -90,12 +107,15 @@ function ordenarJuegos() {
   }
 }
 
-function abrirMenu() {
+function abrirMenu() 
+{
   var x = document.getElementById("miNavbar");
-  if (x.className === "navbar") {
+  if (x.className === "navbar") 
+  {
     x.className += " responsive";
   }
-  else {
+  else 
+  {
     x.className = "navbar";
   }
 } 
