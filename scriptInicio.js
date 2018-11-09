2aa9fc821e0a87e847f2a75c1aacd78c7aaae28f
Renzo Piccardo <renzopiccardo98@gmail.com>
2018-11-08 22:13:51 -0300

    auto JSON!

--- scriptInicio.js
+++ scriptInicio.js
@@ -7,7 +7,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
         for(var i=0; i < jsonParsed.length; i++)
         {
-            ul.innerHTML += "<li><a href='detalles.html?indice=" + i +"'><article class='juego'><figure class='imagenLI'><img src='" + jsonParsed[i].img + "'alt= +""></img><figcaption></figcaption></figure><p></p></article></a></li>";
+            var juego = '';
+            ul.innerHTML += '<li><a href="juegoDetallado.html?indice=' + i +'"><article class="juego"><figure class="imagenLI"><img src="' + jsonParsed[i].imagen + '" alt="' + "portada de " + jsonParsed[i].nombre + '"></img><figcaption> ' + jsonParsed[i].nombre + "</figcaption></figure><p>" + jsonParsed[i].descripcionCorta +"</p></article></a></li>";
         }
 
         /* ul.innerHTML += "<li><a><article><figure><img>" + jsonParsed[i].imagen + "</img><figcaption>" + jsonParsed[i].nombre + "</figcaption></figure><p>" + jsonParsed[i].descripcionCorta + "</p></article></a></li>";} */
