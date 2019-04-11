4b04c3a0e269958a7f2fd4736c35b2f3c7b05e04
Renzo Piccardo <renzopiccardo98@gmail.com>
2019-04-11 00:06:58 -0300

    d

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function cargar2(){
     $("#mainn").addClass('d-none');
     $("#mainn").removeClass('d-block');
     $("#mainn2").removeClass('d-none');
-    setTimeout(fc, 3000);
+    setTimeout(fc, 2200);
 }
 
 function fc(){
