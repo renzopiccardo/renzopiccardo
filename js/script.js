a060e2b12d0c80894d1d7c9baf0a9e0a5be2e761
Renzo Piccardo <renzopiccardo98@gmail.com>
2019-04-11 00:03:22 -0300

    mai

--- js/script.js
+++ js/script.js
@@ -1,41 +1,17 @@
-/*
-
-Funcion que llama una sola vez al json local y lo guarda en el localStorage del navegador
-
-*/
-
-function leerJSONLocal(file, callback) 
-{
-  if (localStorage.getItem("juegos") == undefined) 
-  {
-    var jsonOriginal = new XMLHttpRequest();
-    jsonOriginal.overrideMimeType("application/json");
-    jsonOriginal.open("GET", file, true);
-    jsonOriginal.onreadystatechange = () => 
-    {
-      if (jsonOriginal.readyState === 4 && jsonOriginal.status == "200") 
-      {
-        localStorage.setItem("juegos", jsonOriginal.responseText);
-        callback(jsonOriginal.responseText);
-      }
-    }
-    jsonOriginal.send(null);
-  }
-  else
-  {
-    callback(localStorage.getItem("juegos"));
-  }
+function cargar2(){
+    $("#mainn").addClass('d-none');
+    $("#mainn").removeClass('d-block');
+    $("#mainn2").removeClass('d-none');
+    setTimeout(fc, 2000);
 }
 
-function abrirMenu() 
-{
-  var x = document.getElementById("miNavbar");
-  if (x.className === "navbar") 
-  {
-      x.className += " responsive";
-  } 
-  else 
-  {
-      x.className = "navbar";
-  }
-} 
+function fc(){
+    $("#mainn2").removeClass('d-block');
+    $("#mainn2").addClass('d-none');
+    $("#mainn3").addClass('d-block');
+    $("#mainn3").removeClass('d-none');
+    
+    var confettiSettings = { target: 'my-canvas', size:2, max: 120, animate: true, rotate:true};
+    var confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
+}
